126238633bbb993cf9d9e4871a812ba2a71c6516
Siva <kunchalasiva28@gmail.com>
2025-03-31 00:11:41 +0530

    Student data is added

---
+++ Student.java
@@ -0,0 +1,17 @@
+class Student {
+ private int sno;
+ private String sname;
+ private String course;
+ private double  fee;
+
+Student(int sno,String sname,String course,double fee) {
+  this.sno=sno;
+  this.sname=sname;
+  this.course=course;
+  this.fee=fee;
+}
+
+public  void  display() {
+  System.out.println("Sno "+ sno + "sname " + sname + "course "+ course + "fee " +fee);
+}
+
